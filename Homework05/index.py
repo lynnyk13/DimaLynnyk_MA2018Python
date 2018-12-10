b8deaf7bb9f7024225b4a74fe501efa11a288d59
Dima <lynnyk94@gmail.com>
2018-12-11 01:05:47 +0200

    Homework05

---
+++ Homework05/index.py
@@ -0,0 +1,21 @@
+import simplegui
+import random
+WIDTH = 50
+HTIGHT = 100
+
+numlist = []
+exposeed = []
+moves = 0
+state = 0
+choises = [-1, -1]
+
+
+def init():   
+
+
+def mouseclick(pos):
+
+
+def draw(canvas):
+
+
